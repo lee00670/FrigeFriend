35ec33f9017e5b20005f8d3f7b027716bb715cfa
lee00670 <lee00670@algonquinlive.com>
2018-06-29 11:27:28 -0400

    test

--- app/src/main/java/com/example/jlee/frigefriend/ValidateRequest.java
+++ app/src/main/java/com/example/jlee/frigefriend/ValidateRequest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class ValidateRequest extends StringRequest {
 
-    final static private String URL="http://loacalhost/UserValidate.php";
+    final static private String URL="http://loacalhost/UserValidate.php"; //to check the ip address
     private Map<String, String> parameters;
 
     public ValidateRequest(String userID, Response.Listener<String> listener){
