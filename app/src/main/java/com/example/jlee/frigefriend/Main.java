fe6abdd511c8e66b40a9df2e5cbdfbe2c8ee514e
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-02 16:58:17 -0400

    Main java....I forgot to commit

---
+++ app/src/main/java/com/example/jlee/frigefriend/Main.java
@@ -0,0 +1,15 @@
+package com.example.jlee.frigefriend;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
