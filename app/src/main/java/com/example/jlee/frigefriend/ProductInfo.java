f6f83776a8fcff5a7013018acbaaee3d31e9cae6
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-04 11:46:17 -0400

    Product Info Java added link to Category Activity

--- app/src/main/java/com/example/jlee/frigefriend/ProductInfo.java
+++ app/src/main/java/com/example/jlee/frigefriend/ProductInfo.java
@@ -1,18 +1,23 @@
 package com.example.jlee.frigefriend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.OnClick;
 
 
 public class ProductInfo extends Activity {
@@ -22,6 +27,7 @@ public class ProductInfo extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_info);
 
+//        Displays Quantity in dropdown list
         Spinner quantitySpinner = (Spinner) findViewById(R.id.spinnerQuantity);
 
         ArrayAdapter <String> quantityAdapter = new ArrayAdapter<String>(ProductInfo.this,
@@ -31,7 +37,18 @@ public class ProductInfo extends Activity {
 
         quantitySpinner.setAdapter(quantityAdapter);
 
+//      When Category is clicked directs to CategoryActivity
+        TextView CategoryTextView = (TextView) findViewById(R.id.textViewCategory);
+        CategoryTextView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent CategoryIntent = new Intent(ProductInfo.this, CategoryActivity.class);
+                ProductInfo.this.startActivity(CategoryIntent);
+            }
+        });
 
     }
+
 
 }
