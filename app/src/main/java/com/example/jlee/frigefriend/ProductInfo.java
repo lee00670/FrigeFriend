686731a341d2bf7b140c9014bd14891a63e87e51
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-10 12:07:43 -0400

    Product Info Java file
    1. Added Date picker Dialog Box
    
        a. No errors in code, but when click on Display Date onclick the emulator stops working ?
    
    2. Linked Category TextView to Category Activity

--- app/src/main/java/com/example/jlee/frigefriend/ProductInfo.java
+++ app/src/main/java/com/example/jlee/frigefriend/ProductInfo.java
@@ -1,10 +1,14 @@
 package com.example.jlee.frigefriend;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.app.Activity;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import android.util.Log;
@@ -13,6 +17,7 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +26,11 @@ import butterknife.OnClick;
 
 
 public class ProductInfo extends Activity {
+
+    private static final String TAG = "ProductInfo";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,42 @@ public class ProductInfo extends Activity {
                 ProductInfo.this.startActivity(CategoryIntent);
             }
         });
+
+        // Date picker
+
+        mDisplayDate = findViewById(R.id.textViewDisplayDate);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick (View view) {
+
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dialog = new DatePickerDialog(ProductInfo.this,
+                    android.R.style.TextAppearance_Theme_Dialog,
+                    mDateSetListener,
+                    year, month, day);
+
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+
+        }
+
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month =month +1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
 
     }
 
