54c256db03e119994f9114878da3ef4d37c74fd9
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-16 12:08:31 -0400

    Category Activity Java file
    1. Added Array List with Dairy products and Images

--- app/src/main/java/com/example/jlee/frigefriend/CategoryActivity.java
+++ app/src/main/java/com/example/jlee/frigefriend/CategoryActivity.java
@@ -1,11 +1,15 @@
 package com.example.jlee.frigefriend;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.app.Activity;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ListView;
 import android.view.View;
 import android.widget.TextView;
@@ -17,125 +21,78 @@ import org.json.JSONObject;
 import java.io.InputStream;
 import java.util.Scanner;
 
+import butterknife.OnClick;
+
 public class CategoryActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    String jsonStringUserData;
+    String jsonStringCat;
+    String jsonStringLC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        Intent intent = getIntent();
+        jsonStringUserData = intent.getStringExtra(LoginActivity.USER_DATA);
+        jsonStringCat = intent.getStringExtra(LoginActivity.CAT_DATA);
+        jsonStringLC = intent.getStringExtra(LoginActivity.LC_DATA);
+
+        ArrayList<categoryItem> categoryList = new ArrayList<>();
+        categoryList.add(new categoryItem(R.drawable.milk, "Milk"));
+        categoryList.add(new categoryItem(R.drawable.yogurt, "Yogurt"));
+        categoryList.add(new categoryItem(R.drawable.cheese, "Cheese"));
+        categoryList.add(new categoryItem(R.drawable.butter, "Butter"));
+        categoryList.add(new categoryItem(R.drawable.eggs, "Eggs"));
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new categoryAdapter(categoryList);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        TextView btn = findViewById(R.id.textViewCloseIcon);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CategoryActivity.this, ProductInfo.class));
+            }
+        });
 
 
-//        ArrayList items = new ArrayList();
-//
-//        items.add(new ListCell("Apple", "Fruit"));
-//        items.add(new ListCell("Banana", "Fruit"));
-//        items.add(new ListCell("Peach", "Fruit"));
-//        items.add(new ListCell("Plum", "Fruit"));
-//        items.add(new ListCell("Orange", "Fruit"));
-//        items.add(new ListCell("Pineapple", "Fruit"));
-//        items.add(new ListCell("Pear", "Fruit"));
-//        items.add(new ListCell("Cucummber", "Vegetables"));
-//        items.add(new ListCell("Carrot", "Vegetables"));
-//        items.add(new ListCell("Eggplant", "Vegetables"));
-//        items.add(new ListCell("Tomato", "Vegetables"));
-//        items.add(new ListCell("Potato", "Vegetables"));
-//        items.add(new ListCell("Spinach", "Vegetables"));
-//        items.add(new ListCell("Beets", "Vegetables"));
-//        items.add(new ListCell("Milk", "Dairy Product"));
-//        items.add(new ListCell("Yogurt", "Dairy Product"));
-//        items.add(new ListCell("Cheese", "Dairy Product"));
-//        items.add(new ListCell("Chicken", "Meat"));
-//        items.add(new ListCell("Beef", "Meat"));
-//        items.add(new ListCell("pork", "Meat"));
-//        items.add(new ListCell("Turkey", "Meat"));
-//        items.add(new ListCell("Sausage", "Meat"));
-//        items.add(new ListCell("Juice", "Beverages"));
-//        items.add(new ListCell("Soda", "Beverages"));
-//        items.add(new ListCell("Coffee", "Beverages"));
-//        items.add(new ListCell("Tea", "Beverages"));
-//
-//
-//
-//
-//        ListView list = (ListView) findViewById(R.id.awesome_list);
-//
-//        items = sortAndAddSections(items);
-//
-//        ListAdapter adapter = new ListAdapter(this, items);
-//        list.setAdapter(adapter);
-//
+        View btnAdd = findViewById(R.id.Button_fab);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CategoryActivity.this, ProductInfo.class));
+            }
+        });
+
     }
-//
-//
-//
-//    private ArrayList sortAndAddSections(ArrayList itemList)
+
+
+
+//    @OnClick(R.id.Button_fab)
+//    public void addItem()
 //    {
-//
-//        ArrayList tempList = new ArrayList();
-//        //First we sort the array
-//        Collections.sort(itemList);
-//
-//        //Loops thorugh the list and add a section before each sectioncell start
-//        String header = "";
-//        for(int i = 0; i < itemList.size(); i++)
-//        {
-//            //If it is the start of a new section we create a new listcell and add it to our array
-//            if(header != itemList.get(i).getCategory()){
-//                ListCell sectionCell = new ListCell(itemList.get(i).getCategory(), null);
-//                sectionCell.setToSectionHeader();
-//                tempList.add(sectionCell);
-//                header = itemList.get(i).getCategory();
-//            }
-//            tempList.add(itemList.get(i));
-//        }
-//
-//        return tempList;
-//
+//        Intent addItemIntent = new Intent(this, ProductInfo.class);
+//        addItemIntent.putExtra(LoginActivity.USER_DATA, jsonStringUserData);
+//        addItemIntent.putExtra(LoginActivity.CAT_DATA, jsonStringCat);
+//        addItemIntent.putExtra(LoginActivity.LC_DATA, jsonStringLC);
+//        startActivity(addItemIntent);
 //    }
 
-//    public void loadItems(View view) {
-//
-//        Resources res = getResources();
-//
-//        InputStream is = res.openRawResource(R.raw.items);
-//
-//        Scanner scanner = new Scanner(is);
-//
-//        StringBuilder builder = new StringBuilder();
-//
-//        while (scanner.hasNextLine()) {
-//            builder.append(scanner.nextLine());
-//
-//        }
-//
-//        parseJson(builder.toString());
-//    }
-//
-//    private void parseJson(String s) {
-//        TextView txtDisplay = findViewById(R.id.text_display);
-//
-//        StringBuilder builder = new StringBuilder();
-//        try {
-//            JSONObject root = new JSONObject(s);
-//
-//          JSONObject items = root.getJSONObject("Items");
-//
-//
-//            JSONArray products = items.getJSONArray("foodItems");
-//
-//            for (int i =0; i < products.length(); i++) {
-//                JSONObject product = products.getJSONObject(i);
-//                builder.append(product.getString("name"))
-//                        .append(": ")
-//                        .append(product.getString("category"))
-//                        .append("\n");
-//            }
-//
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//        txtDisplay.setText(s);
    }
+
 
 
 
