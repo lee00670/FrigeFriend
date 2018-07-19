fdb3b73bbc7a57405e360cfc20b0a02aabe36686
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-19 11:20:49 -0400

    Category Item Java file

--- app/src/main/java/com/example/jlee/frigefriend/categoryItem.java
+++ app/src/main/java/com/example/jlee/frigefriend/categoryItem.java
@@ -3,6 +3,7 @@ package com.example.jlee.frigefriend;
 public class categoryItem {
     private int mImageResource;
     private String mText1;
+    public int mImageCheck;
 
     public categoryItem(int imageRecourse, String text1){
 
@@ -10,9 +11,23 @@ public class categoryItem {
         mText1=text1;
     }
 
+//    public categoryItem(int milk, String milk1) {
+//    }
+
+    public void changetext1 (String text){
+        mText1 = text;
+    }
+
+   public void displayCheck (int image) {
+        mImageCheck = image;
+    }
+
+
+
     public int getmImageResource(){
         return mImageResource;
     }
+
 
     public String getmText1(){
         return mText1;
