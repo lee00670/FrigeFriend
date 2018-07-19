f5afff0cd36073420637acde596c66297c45bfd5
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-19 11:20:28 -0400

    Category Adapter Java file

--- app/src/main/java/com/example/jlee/frigefriend/categoryAdapter.java
+++ app/src/main/java/com/example/jlee/frigefriend/categoryAdapter.java
@@ -14,16 +14,42 @@ import java.util.ArrayList;
 public class categoryAdapter extends RecyclerView.Adapter<categoryAdapter.CategoryViewHolder>{
 
     private  ArrayList<categoryItem> mcategoryList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener =listener;
+    }
 
     public static class CategoryViewHolder extends RecyclerView.ViewHolder{
         public ImageView mImageView;
         public TextView mTextView1;
+//        public ImageView mImageCheck;
 
-        public CategoryViewHolder(@NonNull View itemView) {
+
+        public CategoryViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
             super(itemView);
 
             mImageView = itemView.findViewById(R.id.imageview1);
             mTextView1 = itemView.findViewById(R.id.textView1);
+//            mImageCheck = itemView.findViewById(R.id.imageViewCheckMark);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if(position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+
+                }
+            });
         }
     }
 
@@ -35,7 +61,7 @@ public class categoryAdapter extends RecyclerView.Adapter<categoryAdapter.Catego
     @Override
     public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.category_item, viewGroup, false);
-        CategoryViewHolder cvh = new CategoryViewHolder(v);
+        CategoryViewHolder cvh = new CategoryViewHolder(v, mListener);
         return cvh;
     }
 
@@ -45,6 +71,7 @@ public class categoryAdapter extends RecyclerView.Adapter<categoryAdapter.Catego
 
         categoryViewHolder.mImageView.setImageResource(currentItem.getmImageResource());
         categoryViewHolder.mTextView1.setText(currentItem.getmText1());
+
 
     }
 
