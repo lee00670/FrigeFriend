fd540cb4ab7bd2729d83b0eb14c24a124a5b8de0
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-23 15:46:57 -0400

    Add Item Java

---
+++ app/src/main/java/com/example/jlee/frigefriend/addItem.java
@@ -0,0 +1,20 @@
+package com.example.jlee.frigefriend;
+
+public class addItem {
+    private int mImageResource;
+    private String mText1;
+
+    public addItem(int imageResource, String text1) {
+        mImageResource = imageResource;
+        mText1 = text1;
+
+    }
+
+    public int getimageResource() {
+        return mImageResource;
+    }
+
+    public String getText1(){
+        return mText1;
+    }
+} // END OF public class addItem
