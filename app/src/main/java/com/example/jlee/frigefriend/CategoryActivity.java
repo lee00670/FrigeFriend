578873d9e23d654e2f559274de63da9aef4f9c7f
haads23 <32466548+haads23@users.noreply.github.com>
2018-07-30 12:27:37 -0400

    Category  Activity
    1. only display the category names

--- app/src/main/java/com/example/jlee/frigefriend/CategoryActivity.java
+++ app/src/main/java/com/example/jlee/frigefriend/CategoryActivity.java
@@ -47,56 +47,86 @@ public class CategoryActivity extends Activity {
         jsonStringCat = intent.getStringExtra(LoginActivity.CAT_DATA);
         jsonStringLC = intent.getStringExtra(LoginActivity.LC_DATA);
 
-//        findViewById(R.id.headerDairy);
 
         final ArrayList<categoryItem> categoryList = new ArrayList<>();
-        categoryList.add(new categoryItem(R.drawable.milk, "Milk"));
-        categoryList.add(new categoryItem(R.drawable.yogurt, "Yogurt"));
-        categoryList.add(new categoryItem(R.drawable.cheese, "Cheese"));
-        categoryList.add(new categoryItem(R.drawable.butter, "Butter"));
-        categoryList.add(new categoryItem(R.drawable.eggs, "Eggs"));
 
-       categoryList.add(new categoryItem(R.drawable.pork, "Beef"));
-       categoryList.add(new categoryItem(R.drawable.chicken, "Chicken"));
-       categoryList.add(new categoryItem(R.drawable.pork, "Lamb"));
-       categoryList.add(new categoryItem(R.drawable.chicken, "Turkey"));
-       categoryList.add(new categoryItem(R.drawable.pork, "Pork"));
-       categoryList.add(new categoryItem(R.drawable.sausague, "Sausages"));
-       categoryList.add(new categoryItem(R.drawable.bacon, "Bacon"));
-       categoryList.add(new categoryItem(R.drawable.salami, "Salami"));
-       categoryList.add(new categoryItem(R.drawable.fish, "Fish"));
-       categoryList.add(new categoryItem(R.drawable.othermeat, "Other Meat"));
+        categoryList.add(new categoryItem(R.drawable.dairy, "Dairy Products"));
+        categoryList.add(new categoryItem(R.drawable.otherfruits, "Fruits"));
+        categoryList.add(new categoryItem(R.drawable.otherveggies, "Vegetables"));
+        categoryList.add(new categoryItem(R.drawable.othermeat, "Meat Products"));
+        categoryList.add(new categoryItem(R.drawable.otherbakery, "Bakery & Sweets"));
+        categoryList.add(new categoryItem(R.drawable.otherbeverages, "Beverages"));
+        categoryList.add(new categoryItem(R.drawable.otherfood, "Other"));
 
-       categoryList.add(new categoryItem(R.drawable.apple, "Apple"));
-       categoryList.add(new categoryItem(R.drawable.pineapple, "Pineapple"));
-       categoryList.add(new categoryItem(R.drawable.pear, "Pear"));
-       categoryList.add(new categoryItem(R.drawable.orange, "Oranges"));
-       categoryList.add(new categoryItem(R.drawable.lemon, "Lemon"));
-       categoryList.add(new categoryItem(R.drawable.melon,"Melon"));
-       categoryList.add(new categoryItem(R.drawable.kiwi, "Kiwi"));
-       categoryList.add(new categoryItem(R.drawable.grapes, "Grapes"));
-       categoryList.add(new categoryItem(R.drawable.strawberry, "Strawberries"));
-       categoryList.add(new categoryItem(R.drawable.berries, "Berries"));
-       categoryList.add(new categoryItem(R.drawable.avocado, "Avocado"));
-       categoryList.add(new categoryItem(R.drawable.otherfruits, "Other Fruit"));
+//        categoryList.add(new categoryItem(R.drawable.milk, "Milk"));
+//        categoryList.add(new categoryItem(R.drawable.yogurt, "Yogurt"));
+//        categoryList.add(new categoryItem(R.drawable.cheese, "Cheese"));
+//        categoryList.add(new categoryItem(R.drawable.butter, "Butter"));
+//        categoryList.add(new categoryItem(R.drawable.eggs, "Eggs"));
+//
+//       categoryList.add(new categoryItem(R.drawable.pork, "Beef"));
+//       categoryList.add(new categoryItem(R.drawable.chicken, "Chicken"));
+//       categoryList.add(new categoryItem(R.drawable.pork, "Lamb"));
+//       categoryList.add(new categoryItem(R.drawable.chicken, "Turkey"));
+//       categoryList.add(new categoryItem(R.drawable.pork, "Pork"));
+//       categoryList.add(new categoryItem(R.drawable.sausague, "Sausages"));
+//       categoryList.add(new categoryItem(R.drawable.bacon, "Bacon"));
+//       categoryList.add(new categoryItem(R.drawable.salami, "Salami"));
+//       categoryList.add(new categoryItem(R.drawable.fish, "Fish"));
+//       categoryList.add(new categoryItem(R.drawable.othermeat, "Other Meat"));
+//
+//       categoryList.add(new categoryItem(R.drawable.apple, "Apple"));
+//       categoryList.add(new categoryItem(R.drawable.pineapple, "Pineapple"));
+//       categoryList.add(new categoryItem(R.drawable.pear, "Pear"));
+//       categoryList.add(new categoryItem(R.drawable.orange, "Oranges"));
+//       categoryList.add(new categoryItem(R.drawable.lemon, "Lemon"));
+//       categoryList.add(new categoryItem(R.drawable.melon,"Melon"));
+//       categoryList.add(new categoryItem(R.drawable.kiwi, "Kiwi"));
+//       categoryList.add(new categoryItem(R.drawable.grapes, "Grapes"));
+//       categoryList.add(new categoryItem(R.drawable.strawberry, "Strawberries"));
+//       categoryList.add(new categoryItem(R.drawable.berries, "Berries"));
+//       categoryList.add(new categoryItem(R.drawable.avocado, "Avocado"));
+//       categoryList.add(new categoryItem(R.drawable.otherfruits, "Other Fruit"));
+//
+//       categoryList.add(new categoryItem(R.drawable.cucummber, "Cucumber"));
+//       categoryList.add(new categoryItem(R.drawable.broccoli, "Broccoli"));
+//       categoryList.add(new categoryItem(R.drawable.carrots, "Carrots"));
+//       categoryList.add(new categoryItem(R.drawable.pepper, "Pepper"));
+//       categoryList.add(new categoryItem(R.drawable.lettuce, "Lettuce"));
+//       categoryList.add(new categoryItem(R.drawable.tomato, "Tomatoes"));
+//       categoryList.add(new categoryItem(R.drawable.potato, "Potatoes"));
+//       categoryList.add(new categoryItem(R.drawable.mushroom, "Mushrooms"));
+//       categoryList.add(new categoryItem(R.drawable.garlic, "Garlic"));
+//       categoryList.add(new categoryItem(R.drawable.ginger, "Ginger"));
+//       categoryList.add(new categoryItem(R.drawable.onion, "Onions"));
+//       categoryList.add(new categoryItem(R.drawable.chili, "Chili"));
+//       categoryList.add(new categoryItem(R.drawable.corn, "Corn"));
+//       categoryList.add(new categoryItem(R.drawable.peas, "Peas"));
+//       categoryList.add(new categoryItem(R.drawable.eggplant, "Eggplant"));
+//       categoryList.add(new categoryItem(R.drawable.herbs, "Herbs"));
+//       categoryList.add(new categoryItem(R.drawable.otherveggies, "Other Vegetables"));
+//
+//        categoryList.add(new categoryItem(R.drawable.bread, "Bread"));
+//        categoryList.add(new categoryItem(R.drawable.bagel, "Bagel"));
+//        categoryList.add(new categoryItem(R.drawable.donuts, "Doughnuts"));
+//        categoryList.add(new categoryItem(R.drawable.cake, "Cake"));
+//        categoryList.add(new categoryItem(R.drawable.cookies, "Cookies"));
+//        categoryList.add(new categoryItem(R.drawable.chocolate, "Chocolate"));
+//        categoryList.add(new categoryItem(R.drawable.pie, "Pie"));
+//        categoryList.add(new categoryItem(R.drawable.icecream, "Ice Cream"));
+//        categoryList.add(new categoryItem(R.drawable.otherbakery, "Other Bakery&Sweets"));
+//
+//        categoryList.add(new categoryItem(R.drawable.softdrink, "Soft Drinks"));
+//        categoryList.add(new categoryItem(R.drawable.soda, "Soda"));
+//        categoryList.add(new categoryItem(R.drawable.jucie, "Juice"));
+//        categoryList.add(new categoryItem(R.drawable.icedtea, "Iced Tea"));
+//        categoryList.add(new categoryItem(R.drawable.coffee, "Coffee"));
+//        categoryList.add(new categoryItem(R.drawable.water, "Water"));
+//        categoryList.add(new categoryItem(R.drawable.smoothie, "Smoothies"));
+//        categoryList.add(new categoryItem(R.drawable.otherbeverages, "Other Beverages"));
 
-       categoryList.add(new categoryItem(R.drawable.cucummber, "Cucumber"));
-       categoryList.add(new categoryItem(R.drawable.broccoli, "Broccoli"));
-       categoryList.add(new categoryItem(R.drawable.carrots, "Carrots"));
-       categoryList.add(new categoryItem(R.drawable.pepper, "Pepper"));
-       categoryList.add(new categoryItem(R.drawable.lettuce, "Lettuce"));
-       categoryList.add(new categoryItem(R.drawable.tomato, "Tomatoes"));
-       categoryList.add(new categoryItem(R.drawable.potato, "Potatoes"));
-       categoryList.add(new categoryItem(R.drawable.mushroom, "Mushrooms"));
-       categoryList.add(new categoryItem(R.drawable.garlic, "Garlic"));
-       categoryList.add(new categoryItem(R.drawable.ginger, "Ginger"));
-       categoryList.add(new categoryItem(R.drawable.onion, "Onions"));
-       categoryList.add(new categoryItem(R.drawable.chili, "Chili"));
-       categoryList.add(new categoryItem(R.drawable.corn, "Corn"));
-       categoryList.add(new categoryItem(R.drawable.peas, "Peas"));
-       categoryList.add(new categoryItem(R.drawable.eggplant, "Eggplant"));
-       categoryList.add(new categoryItem(R.drawable.herbs, "Herbs"));
-       categoryList.add(new categoryItem(R.drawable.otherveggies, "Other Vegetables"));
+
+
 
         mRecyclerView = findViewById(R.id.recyclerView);
         mLayoutManager = new LinearLayoutManager(this);
@@ -111,8 +141,8 @@ public class CategoryActivity extends Activity {
         mAdapter.setOnItemClickListener(new categoryAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
-                categoryList.get(position).changetext1("Clicked");
-                categoryList.get(position).displayCheck(R.drawable.ic_check_black_24dp);
+                  categoryList.get(position).changetext1("Clicked");
+//                categoryList.get(position).displayCheck(R.drawable.ic_check_black_24dp);
                 mAdapter.notifyItemChanged(position);
             }
         });
@@ -138,7 +168,7 @@ public class CategoryActivity extends Activity {
             }
         });
 
-    }
+    } // END of protected void onCreate(Bundle savedInstanceState)
 
 
 
@@ -154,7 +184,7 @@ public class CategoryActivity extends Activity {
 
 
 
-   }
+   } // END of public class CategoryActivity extends Activity
 
 
 
